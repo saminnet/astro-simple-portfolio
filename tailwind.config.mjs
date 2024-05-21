06ed65b5608b972785fcc52ba7bfb45adbafc71d
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-05-21 15:53:48 +0530

    feat: astro-font - optimize fonts & prevent CLS (#2)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,9 +5,6 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			fontFamily: {
-				sora: ['Sora Variable', ...defaultTheme.fontFamily.sans],
-			},
 			backgroundImage: {
 				horizon: "url('/assets/svg/backgrounds/horizon.svg')",
 			},
