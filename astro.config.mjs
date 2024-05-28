53b25626810c3ccb56704543269c6386562c009b
Samin Yousefnia <samin@live.fi>
2024-05-28 13:07:03 +0300

    chore: update deps and configs (#4)
    - Update Astro to version 4.9.2
    - Update Astro Sitemap to version 3.1.5
    - Update lint-staged to version 15.2.5
    - Use `hybrid` mode for output

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://astro-simple-portfolio.vercel.app/', // Required for sitemap -> Replace with your site's URL
-	output: 'server',
+	output: 'hybrid',
 	integrations: [tailwind(), sitemap()],
 	adapter: vercel(),
 });
