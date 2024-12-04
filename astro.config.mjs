da27ea341da9c1de01fb0aa0902175ef57daf5e9
Samin Yousefnia <hello@samin.dev>
2024-12-04 13:04:49 +0330

    chore(deps): bump astro to next major version 5.0.2
    update dependencies and change output format to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://astro-simple-portfolio.vercel.app/', // Required for sitemap -> Replace with your site's URL
-	output: 'hybrid',
+	output: 'static',
 	integrations: [tailwind(), sitemap()],
 	adapter: vercel(),
 });
